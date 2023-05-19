6d19d1dfe314f21157a96523e7eb5e3a9a83ad2e
MuhammadNaufalNurIrawan <129809864+ZaeNau@users.noreply.github.com>
2023-05-20 05:35:19 +0700

    update

--- Base_Project/app.py
+++ Base_Project/app.py
@@ -1,5 +1,28 @@
 from app import app
 
-app.run(
-    debug = True
-)
+def main():
+    while True:
+        print_menu()
+        choice = input("Enter your choice: ")
+        
+        if choice == '1':
+            while True:
+                print_user_menu()
+                user_choice = input("Enter your choice: ")
+                if user_choice == '5':
+                    break
+                handle_user_menu_choice(user_choice)
+        elif choice == '2':
+            while True:
+                print_role_menu()
+                role_choice = input("Enter your choice: ")
+                if role_choice == '5':
+                    break
+                handle_role_menu_choice(role_choice)
+        elif choice == '3':
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
